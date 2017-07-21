e3dd8ea1bf57791153c4eeba32c4130ac07bb112
Jess Champion <jess@silverstripe.com>
2017-07-21 15:00:02 +1200

    Inital commit - Class example

---
+++ cartClass.js
@@ -0,0 +1,58 @@
+/*
+* Class example
+* A very simple shopping cart model
+* */
+
+// Get total functions
+// ----------------------------------------------
+const getItemTotal = item => item.price * item.quantity;
+
+const sum = (total, value) => total + value;
+
+const getTotal = (items) => {
+  if (items.length === 0) {
+    return 0;
+  }
+  const totals = items.map(getItemTotal);
+  return totals.reduce(sum);
+};
+
+// Add item functions
+// ----------------------------------------------
+const matchesById = targetId => item => item.id === targetId;
+
+const addQuantityToExistingItem = (index, quantity, items) => {
+  items[index].quantity += quantity;
+  return items;
+};
+
+const addNewItem = (newItem, items) => {
+  items.push(newItem);
+  return items;
+};
+
+const addItem = (newItem, items) => {
+  const index = items.findIndex(matchesById(newItem.id));
+  if (index >= 0) {
+    return addQuantityToExistingItem(index, newItem.quantity, items);
+  }
+  return addNewItem(newItem, items);
+};
+
+// Public interface
+// ----------------------------------------------
+
+class Cart {
+  constructor(items = []) {
+    const _items = [...items];
+    this.items = _items;
+    this.total = getTotal(_items);
+  }
+
+  // add an item to the cart
+  // ----------------------------------------------
+  addItem(newItem) {
+    this.items = addItem(newItem, this.items);
+    this.total = getTotal(this.items);
+  }
+}
---
+++ cartClass.spec.js
@@ -0,0 +1,95 @@
+const getTestCartWithItems = () => new Cart([{
+  id: 1,
+  price: 2,
+  quantity: 3,
+}, {
+  id: 2,
+  price: 2.5,
+  quantity: 4,
+}]);
+
+describe("A shopping cart", () => {
+  it("creates an new empty cart", () => {
+    const testCart = new Cart([]);
+    expect(testCart.items.length).toBe(0);
+    expect(testCart.total).toBe(0);
+  });
+
+  it("creates an new cart with items", () => {
+    const testCart = getTestCartWithItems();
+    expect(testCart.items.length).toBe(2);
+    expect(testCart.total).toBe(16);
+  });
+
+  describe("add item to the cart", () => {
+    it("can add an item to and empty cart and calculate the total", () => {
+      const testCart = new Cart([]);
+      const testItem = {
+        id: 3,
+        price: 2.5,
+        quantity: 2
+      };
+
+      testCart.addItem(testItem);
+
+      expect(testCart.items.length).toBe(1);
+      expect(testCart.items[0]).toEqual(testItem);
+      expect(testCart.total).toBe(5);
+    });
+
+    it("can add an item of a new type to the cart", () => {
+      const testCart = getTestCartWithItems();
+      const testItem = {
+        id: 3,
+        price: 2.5,
+        quantity: 2
+      };
+
+      testCart.addItem(testItem);
+
+      expect(testCart.items.length).toBe(3);
+      expect(testCart.items[2]).toEqual(testItem);
+    });
+
+    it("should correctly calculate the cart total when adding an item of a new type", () => {
+      const testCart = getTestCartWithItems();
+      const testItem = {
+        id: 3,
+        price: 2.5,
+        quantity: 2
+      };
+
+      testCart.addItem(testItem);
+      expect(testCart.items[2]).toEqual(testItem);
+      expect(testCart.total).toBe(21);
+    });
+
+    it("can add another item of an existing type to the cart", () => {
+      const testCart = getTestCartWithItems();
+      const testItem = {
+        id: 2,
+        price: 2.5,
+        quantity: 2,
+      };
+
+      testCart.addItem(testItem);
+
+      expect(testCart.items.length).toBe(2);
+      expect(testCart.items[1].quantity).toBe(6);
+    });
+
+    it("should correctly calculate the cart total when adding an item of an existing type", () => {
+      const testCart = getTestCartWithItems();
+
+      const testItem = {
+        id: 2,
+        price: 2.5,
+        quantity: 5,
+      };
+
+      testCart.addItem(testItem);
+
+      expect(testCart.total).toBe(28.5);
+    });
+  });
+});
